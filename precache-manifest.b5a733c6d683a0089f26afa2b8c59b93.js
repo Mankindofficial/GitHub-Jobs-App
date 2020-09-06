24327bc7b6bd40ff2001852b59a4235b6c7b6464
Makinde Opeyemi <mankindofficial1@gmail.com>
2020-09-06 14:41:16 +0100

    Updates

---
+++ precache-manifest.b5a733c6d683a0089f26afa2b8c59b93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e241a3f508dafcce43531018500674e7",
+    "url": "/GitHub-Jobs-App/index.html"
+  },
+  {
+    "revision": "dc577cc94e4a7e049ac5",
+    "url": "/GitHub-Jobs-App/static/css/main.192e3bd5.chunk.css"
+  },
+  {
+    "revision": "eac604090fe75f74bc40",
+    "url": "/GitHub-Jobs-App/static/js/2.df3f2051.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "/GitHub-Jobs-App/static/js/2.df3f2051.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc577cc94e4a7e049ac5",
+    "url": "/GitHub-Jobs-App/static/js/main.644d79ce.chunk.js"
+  },
+  {
+    "revision": "b973dbf0eb8e4bddd5e0",
+    "url": "/GitHub-Jobs-App/static/js/runtime-main.51bc4384.js"
+  }
+]);
